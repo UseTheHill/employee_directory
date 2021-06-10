57bf48daa7df4cc63344af207490af59cb3d8f26
Kayte McDonough <kaytemcdonough@gmail.com>
2021-06-10 11:28:26 -0400

    created placeholder table until api is running

---
+++ src/components/Table.js
@@ -0,0 +1,104 @@
+import React from "react";
+import Container from "./Container";
+
+function Table() {
+  return (
+    <Container>
+      <div class="table-responsive">
+        <table className="table table-hover text-center">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Location</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">Date of Birth</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">
+                <img src="https://picsum.photos/100" alt="Placeholder" />
+              </th>
+              <td className="align-middle">Kayte McDonough</td>
+              <td className="align-middle">Michigan</td>
+              <td className="align-middle">248-555-1111</td>
+              <td className="align-middle">kayte@gmail.com</td>
+              <td className="align-middle">02/11/1984</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <img src="https://picsum.photos/100" alt="Placeholder" />
+              </th>
+              <td className="align-middle">Kayte McDonough</td>
+              <td className="align-middle">Michigan</td>
+              <td className="align-middle">248-555-1111</td>
+              <td className="align-middle">kayte@gmail.com</td>
+              <td className="align-middle">02/11/1984</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <img src="https://picsum.photos/100" alt="Placeholder" />
+              </th>
+              <td className="align-middle">Kayte McDonough</td>
+              <td className="align-middle">Michigan</td>
+              <td className="align-middle">248-555-1111</td>
+              <td className="align-middle">kayte@gmail.com</td>
+              <td className="align-middle">02/11/1984</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="table-responsive">
+        <table className="table table-hover text-center">
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Location</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">Date of Birth</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">
+                <img src="https://picsum.photos/100" alt="Placeholder" />
+              </th>
+              <td className="align-middle">Kayte McDonough</td>
+              <td className="align-middle">Michigan</td>
+              <td className="align-middle">248-555-1111</td>
+              <td className="align-middle">kayte@gmail.com</td>
+              <td className="align-middle">02/11/1984</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <img src="https://picsum.photos/100" alt="Placeholder" />
+              </th>
+              <td className="align-middle">Kayte McDonough</td>
+              <td className="align-middle">Michigan</td>
+              <td className="align-middle">248-555-1111</td>
+              <td className="align-middle">kayte@gmail.com</td>
+              <td className="align-middle">02/11/1984</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <img src="https://picsum.photos/100" alt="Placeholder" />
+              </th>
+              <td className="align-middle">Kayte McDonough</td>
+              <td className="align-middle">Michigan</td>
+              <td className="align-middle">248-555-1111</td>
+              <td className="align-middle">kayte@gmail.com</td>
+              <td className="align-middle">02/11/1984</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Container>
+  );
+}
+
+export default Table;
